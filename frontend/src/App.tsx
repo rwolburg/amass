a6af8f8bafa099296db3663bdbef196e255467a8
rwolburg <72942623+rwolburg@users.noreply.github.com>
2026-04-02 12:58:33 -0600

    Create App component for Amass GUI dashboard

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Amass GUI Dashboard</h1>
+            <p>Welcome to the Amass GUI Dashboard!</p>
+        </div>
+    );
+};
+
+export default App;
